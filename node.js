55f78060af7e21101847965596b8c1034c9a017d
jck-bit <jckkinyanjui@gmail.com>
2021-03-14 22:23:39 +0300

    event..tulikua na jasmine na nam leo

--- node.js
+++ node.js
@@ -1,52 +1,11 @@
-<<<<<<< HEAD
-/*
-// easier way of creating a route
+// event module
 
-const http = require("http");
+const events = require("events");
 
-const routes = {
-    '/':function index(request,response){
-        response.WriteHead(200);
-        response.end("Node Routing");
-    },
-    '/aboutus':function aboutus(request,response){
-        response.end("This is about page");
-    }
-}
+let ev = new events.EventEmmitter();
 
-http.createServer(function(req,res){
-    if(req.url in routes){
-        return routes(req.url)(req,res);
-    }
-}).listen(process.env.PORT || 8000);
-*/
+ev.on('my_event',function(data){
+    console.log("Event :",data);
+})
 
-// a path module
-const path = require("path");
-
-let dir = "learning node"
-
-file = path.join('c:','Users/jack/Desktop',dir,'test.json');
-console.log(file);
-=======
-// easier way of creating a route
-
-const http = require("http");
-
-const routes = {
-    '/':function index(request,response){
-        response.WriteHead(200);
-        response.end("Node Routing");
-    },
-    '/aboutus':function aboutus(request,response){
-        response.end("This is about page");
-    }
-}
-
-http.createServer(function(req,res){
-    if(req.url in routes){
-        return routes(req.url)(req,res);
-    }
-}).listen(8000);
-
->>>>>>> 83ad99f75e4d6c72ca6a8147369620c1abacd563
+ev.emit('my_event','call emit() method to fire my_event');
