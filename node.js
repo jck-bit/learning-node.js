365775f91c1644b9f6ab0fbed77955fe8f877ea4
spacious_2000 <59308363+jck-bit@users.noreply.github.com>
2021-03-15 23:33:45 +0300

    Add files via upload
    streams in node.js

--- node.js
+++ node.js
@@ -1,9 +1,22 @@
+/*
 // event module
 
 const events = require("events");
 
 let ev = new events.EventEmmitter();
 
-ev.once('eventonce',() =>console.log("eventones once fired"))
+ev.once('eventOnce',() => console.log("events once fired"));
 
-ev.emit('eventonce');
+ev.emit('eventOnce');
+*/
+//streams 
+
+const http = require("http");
+const fs = require("fs")
+
+const server = http.createServer(function(req,res){
+    const stream = fs.createReadStream('test.json');
+    stream.pipe(res);
+  
+})
+server.listen(3000,() =>(console.log("application started on port 3000")));
