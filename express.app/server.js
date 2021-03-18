9a8577a9d4162f3ea1457b1d2fb57f398aaa539c
jck-bit <jckkinyanjui@gmail.com>
2021-03-18 17:11:11 +0300

    started a server using express.js

---
+++ express.app/server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express();
+
+
+app.get("/ping",(req,res)=>{
+    res.send("node express application")
+});
+
+app.listen(3000,() =>{console.log("server started on port 3000")});
