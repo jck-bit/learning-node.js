44344796d520ab1af5336c598c00c6f678af0390
spacious_2000 <jckkinyanjui@gmail.com>
2021-04-09 21:01:09 +0300

    middleware functions

--- express.app/server.js
+++ express.app/server.js
@@ -1,9 +1,15 @@
-const express = require("express")
-const app = express();
+//middleware function
+const express = require("express");
+const app = express(); 
 
+const mylogger = function(req, res, next){
+    console.log("LOGGED");
+    next();
+}
 
-app.get("/ping",(req,res)=>{
-    res.send("node expre")
+app.use(mylogger)
+
+app.get('/',function(req,res){
+    res.send("home Route");
 })
-
-app.listen(3000,()=>{console.log('server started on port 3000')});
+app.listen(3000,()=>console.log("App started on PORT 3000"))
