538810d405319fca0ef01b60af4cdadb680441e2
spacious_2000 <jckkinyanjui@gmail.com>
2021-04-14 23:12:35 +0300

    views in node.js

--- express.app/server.js
+++ express.app/server.js
@@ -1,26 +1,8 @@
-//express routing 
- 
+//views
+
 
 const express = require("express")
 const app = express();
 
-const PORT = 3000;
+const PORT = 3000; 
 
-const data ={
-    id: 1,
-    name:'kenya'
-
-}
-app.get('/',(req,res)=>{
-    res.end("welcome to my homepage")
-})
-
-app.get('/about',(req,res)=>{
-    res.send("welcome to my about page")
-})
-
-app.get('/weather',(req,res)=>{
-    res.json(data)
-})
-
-app.listen(PORT,()=>{console.log("server is started on port 3000",PORT)})
