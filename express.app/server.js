81933378e1592203a671adbd0cea33f7c64d7437
spacious_2000 <jckkinyanjui@gmail.com>
2021-04-14 23:24:29 +0300

    pug language

--- express.app/server.js
+++ express.app/server.js
@@ -6,3 +6,8 @@ const app = express();
 
 const PORT = 3000; 
 
+app.set('view engine','pug')
+
+app.get('/',function(req,res){
+    res.render('index')
+}).listen(PORT,()=>{console.log('server started on port 3000')});
