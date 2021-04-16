c398f4718fdc1f3396e8d66f20f320aa2dce010b
spacious_2000 <jckkinyanjui@gmail.com>
2021-04-16 21:17:16 +0300

    pug

--- express.app/server.js
+++ express.app/server.js
@@ -6,7 +6,7 @@ const express = require("express")
 const path = require("path")
 const app =express ()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 2000
 
 app.set("views",path.join(__dirname,'views'));
 app.set('view engine','pug')
