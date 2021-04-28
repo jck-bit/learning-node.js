907e185c2e8f7ffa81630b0c62c2844dbd3329e0
jck-bit <jckkinyanjui@gmail.com>
2021-04-28 11:18:42 +0300

    fj

--- express.app/server.js
+++ express.app/server.js
@@ -1,7 +1,5 @@
 //Express form in node.js
-
-//   
-
+ 
 const express = require("express")
 const path = require("path")
 const app =express ()
@@ -11,10 +9,23 @@ const PORT = process.env.PORT || 2000
 app.set("views",path.join(__dirname,'views'));
 app.set('view engine','pug')
 
+app.use(express.urlencoded({
+  extended:true
+}))
+
+
 app.get('/',(req,res)=>{
   res.render("index",{title:"form handling"});  
 })
 
+app.post("/form_submit",(req,res)=>{
+  const username = req.body.username
+  const email = req.body.email
+  res.end("Your username is:$(username) and email is $()")
+})
+
+
 app.listen(PORT,()=>{
+
     console.log('listening to requests on http://localhost:${PORT}');
 })
