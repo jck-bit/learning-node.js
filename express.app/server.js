e3ef140aa0ae58a832f36accb2b4c881635d8993
jck-bit <jckkinyanjui@gmail.com>
2021-05-02 23:56:42 +0300

    session`

--- express.app/server.js
+++ express.app/server.js
@@ -1,31 +1,21 @@
-//Express form in node.js
- 
+//session
 const express = require("express")
-const path = require("path")
-const app =express ()
+const app = express()
+const session = require("express-session")
 
-const PORT = process.env.PORT || 2000
+const PORT = process.env.PORT || 3000
 
-app.set("views",path.join(__dirname,'views'));
-app.set('view engine','pug')
-
-app.use(express.urlencoded({
-  extended:true
+app.use(session({
+  secret:"Your secret key",
+  resave:true,
+  saveUninitialized:true
 }))
 
-
-app.get('/',(req,res)=>{
-  res.render("index",{title:"form handling"});  
+app.get("/",(req,res)=>{
+  var name ='John';
+  return res.send('session set')
 })
-
-app.post("/form_submit",(req,res)=>{
-  const username = req.body.username
-  const email = req.body.email
-  res.end("Your username is:$(username) and email is $()")
-})
-
 
 app.listen(PORT,()=>{
-
-    console.log('listening to requests on http://localhost:${PORT}');
+  console.log('listening the request on http://localhost:$(PORT)')
 })
